89101026aa7e7bba5d59ebedcfcd2c84f14a3cbf
Remita Amine <remitamine@gmail.com>
2020-01-26 16:17:51 +0100

    [svt] fix series extraction(closes #22297)

--- youtube_dl/extractor/svt.py
+++ youtube_dl/extractor/svt.py
@@ -4,11 +4,7 @@ from __future__ import unicode_literals
 import re
 
 from .common import InfoExtractor
-from ..compat import (
-    compat_parse_qs,
-    compat_str,
-    compat_urllib_parse_urlparse,
-)
+from ..compat import compat_str
 from ..utils import (
     determine_ext,
     dict_get,
@@ -16,7 +12,6 @@ from ..utils import (
     str_or_none,
     strip_or_none,
     try_get,
-    urljoin,
 )
 
 
